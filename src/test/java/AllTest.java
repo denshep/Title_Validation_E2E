0732f5728399ad838b4d5c7320f92c89e3be59bd
Denis Shepelev <den.shep@yahoo.com>
2015-03-19 23:19:54 -0700

    added AllTests suite

---
+++ src/test/java/AllTest.java
@@ -0,0 +1,14 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by ds on 3/19/15.
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        SeleniumTest.class
+})
+
+public class AllTest {
+}
